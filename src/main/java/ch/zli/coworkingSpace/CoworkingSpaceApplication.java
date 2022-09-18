043447fe32cc6a1774f4fbda07e922b88551dd8b
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-09-18 17:02:00 +0200

    created initial project files

---
+++ src/main/java/ch/zli/coworkingSpace/CoworkingSpaceApplication.java
@@ -0,0 +1,13 @@
+package ch.zli.coworkingSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoworkingSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoworkingSpaceApplication.class, args);
+	}
+
+}
