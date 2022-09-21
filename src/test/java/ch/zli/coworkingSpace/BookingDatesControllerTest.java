65e96c24c98795c75bb9c086dc9b1e1935e6a91d
Luca Mock <luca.mock@ringieraxelspringer.ch>
2022-09-21 14:12:11 +0200

    corrected integration tests

--- src/test/java/ch/zli/coworkingSpace/BookingDatesControllerTest.java
+++ src/test/java/ch/zli/coworkingSpace/BookingDatesControllerTest.java
@@ -1,27 +1,21 @@
 package ch.zli.coworkingSpace;
 
 import ch.zli.coworkingSpace.model.BookingDatesEntity;
+import ch.zli.coworkingSpace.repository.BookingDatesRepository;
+import ch.zli.coworkingSpace.repository.UserRepository;
 import ch.zli.coworkingSpace.security.JwtServiceHMAC;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.Assertions;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jdk.jfr.Description;
+import lombok.val;
 import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.core.type.TypeReference;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.UUID;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -30,76 +24,40 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 @AutoConfigureMockMvc
 public class BookingDatesControllerTest {
 
-    private MockMvc mockMvc;
-
     @Autowired
-    private WebApplicationContext wac;
-
-    @LocalServerPort
-    private int port;
+    private MockMvc mockMvc;
 
     @Autowired
     private JwtServiceHMAC jwtService;
 
-    private String accessToken;
+    @Autowired
+    private ObjectMapper objectMapper;
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
+    @Autowired
+    private BookingDatesRepository bookingDates;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    private String accessToken;
 
     @BeforeEach
     public void init() {
-        accessToken = jwtService.createNewJWT(UUID.randomUUID().toString(), "1", "email@gmail.com", List.of("ADMIN"));
+        accessToken = jwtService.createNewJWT(UUID.randomUUID().toString(), "13", "Liselotte.Muster@mail.com", List.of("ADMIN"));
     }
 
-    BookingDatesEntity bookEntity1 = new BookingDatesEntity();
-
-    BookingDatesEntity bookEntity2 = new BookingDatesEntity();
-
-    List<BookingDatesEntity> bookEntityList = new ArrayList<>();
-
-    private String createURLWithPort(String uri) {
-        return "http://localhost:" + port + uri;
-    }
-
-    HttpHeaders headers = new HttpHeaders();
-
-    @Before("")
-    public void setup() throws Exception {
-        MockitoAnnotations.initMocks(this);
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
-
-        bookEntity1.setId(Long.valueOf(1));
-        bookEntity1.setBooked(false);
-        bookEntity1.setDate(new Date(12-14-2022));
-        bookEntity1.setUser_id(1);
-        bookEntity1.setPrice(120);
-        bookEntity1.setWholeDay(true);
-        bookEntityList.add(bookEntity1);
-        bookEntityList.add(bookEntity2);
-    }
-
-    @Test  //@GET("/bookings")
-    public void givenBookEntity_whenGetAllBook_thenStatus200()
-            throws Exception {
-
-        mockMvc.perform(get("/bookings")
-                        .contentType(MediaType.APPLICATION_JSON))
+    @Test
+    @Description("Tests if all entries in the inital db are reachable")
+    public void allBookingsShouldBeReturnedFromService() throws Exception {
+        val response = mockMvc.perform(get("/bookings").header("Authorization", "Bearer " + accessToken))
                 .andExpect(status().isOk())
-                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
-                .andExpect(MockMvcResultMatchers.status().is2xxSuccessful());
+                .andReturn();
+
+        List<BookingDatesEntity> bookings = objectMapper.readValue(response.getResponse().getContentAsString(), new TypeReference<>() {
+        });
+
+        assertEquals(4, bookings.size());
     }
 
-    @Test //@GET("/bookings/{id}")
-    public void givenBookDTO_whenCallingControllerFindIdBy_thenStatus200()
-            throws Exception {
-        HttpEntity<BookingDatesEntity> entity = new HttpEntity<BookingDatesEntity>(null, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                createURLWithPort("/bookings/1"),
-                HttpMethod.GET, entity, String.class);
-
-        String expected = "{\"id\":1,\"date\":\"12-14-2022\",\"whole_day\":\"true\",\"booked\":\"true\", \"user_id\":\"1\", \"price\":\"120\"}";
-
-        Assertions.assertEquals(expected, String.valueOf(response.getBody()));
-    }
 
 }
